75db61acd31196cde52973fb0d79732b26682a00
bmhungqb <21522110@gm.uit.edu.vn>
2024-01-01 09:04:44 +0700

    fix

--- Code/reference.py
+++ Code/reference.py
@@ -1,31 +1,38 @@
 from ultralytics import YOLO
 import cv2
-from tools.dehaze import haze_removal
+import  tools.dark_channel_prior as dcp
 import numpy as np
-# Load YOLO model
-model = YOLO(r"Code/weights/best.pt")
 
-# Define path to video file
-source = r"Code/assets/video_demo.mp4"
+def remove_noise(image):
+    processed_image, alpha_map = dcp.haze_removal(image, w_size=15, a_omega=0.95, gf_w_size=200, eps=1e-6)
+    return processed_image
 
-cap = cv2.VideoCapture(source)
+def main():
+    # Load YOLO model
+    model = YOLO(pth_model)
 
-# Define the codec and create a video writer object for MP4
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-output_path = 'output_video.mp4'
-out = cv2.VideoWriter(output_path, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+    cap = cv2.VideoCapture(pth_source)
 
-# Loop through the video frames
-while cap.isOpened():
-    # Read a frame from the video
-    success, frame = cap.read()
-    processed_image, alpha_map = haze_removal(frame, w_size=15, a_omega=0.95, gf_w_size=200, eps=1e-6)
-    cv2.imshow("img", processed_image)
-    if success:
-        # Run YOLOv8 inference on the frame
-        results = model(frame)
+    # Define the codec and create a video writer object for MP4
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(pth_output, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
 
-        # Visualize the results on the frame
-        annotated_frame = results[0].plot()
-        # Write the annotated frame to the output video
-        out.write(annotated_frame)
+    # Loop through the video frames
+    while cap.isOpened():
+        # Read a frame from the video
+        success, frame = cap.read()
+
+        processed_image = remove_noise(frame)
+        if success:
+            # Run YOLOv8 inference on the frame
+            results = model(processed_image)
+            print(results)
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+            # Write the annotated frame to the output video
+            out.write(annotated_frame)
+if __name__ == "__main__":
+    pth_model = r"Code/weights/best.pt"
+    pth_source = r"Code/assets/video_demo.mp4"
+    pth_output = r"output_video.mp4"
+    main()
